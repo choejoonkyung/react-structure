bf31c68adf83f38ca2eb1bd72d2ff0f73caa2de8
unknown <choejunkyung@gmail.com>
2021-10-16 15:14:23 +0900

    react-router-dom 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
-import React from "react";
+import { Link, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <p>hello</p>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/test">Test</Link>
+          </li>
+        </ul>
+      </nav>
+      <Switch>
+        <Route path="/test"></Route>
+      </Switch>
     </div>
   );
 }
