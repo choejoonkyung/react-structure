ec1b21e2fe98e99703bf2776b80121af83b37e52
choejoonkyung <choejunkyung@gmail.com>
2021-10-17 17:20:27 +0900

    Input 컴포넌트 디바운스 처리

--- src/components/@base/Input/index.tsx
+++ src/components/@base/Input/index.tsx
@@ -1,7 +1,13 @@
+import { debounce } from "lodash";
+import React from "react";
+
 type InputProps = {} & React.InputHTMLAttributes<HTMLInputElement>;
 
-function Input({ ...rest }: InputProps) {
-  return <input {...rest} />;
+function Input({ onChange, ...rest }: InputProps) {
+  const debounceChange = (e: React.ChangeEvent<HTMLInputElement>) =>
+    debounce(() => onChange!(e), 300)();
+
+  return <input onChange={debounceChange} {...rest} />;
 }
 
 export default Input;
