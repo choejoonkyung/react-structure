36e75c2a0fa50159d774536817341e4c494e3bd8
choejoonkyung <choejunkyung@gmail.com>
2021-11-02 19:38:36 +0900

    error 핸들링 위치 설정

--- src/components/@base/Test/ErrorHandle.tsx
+++ src/components/@base/Test/ErrorHandle.tsx
@@ -1,13 +1,21 @@
 import TestingService from "lib/api/TestingService";
+import useErrorBoundary from "lib/Errors/useErrorBoundary";
 
 type ErrorHandleProps = {};
 
 function ErrorHandle({}: ErrorHandleProps) {
+  const setError = useErrorBoundary();
+
   const fetchData = async () => {
     try {
-      await TestingService.fetchError();
+      const result = await TestingService.fetchError();
+      console.log(result);
     } catch (error) {
-      throw error;
+      console.log(error);
+      //custom 에러처리
+      if (error instanceof Error) {
+        setError(error);
+      }
     }
   };
 
